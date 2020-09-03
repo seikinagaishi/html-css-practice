79449e139002d54ae069ae66cfc36a2f2a2adcac
Seiki Nagaishi <51789589+seikinagaishi@users.noreply.github.com>
2020-09-03 18:23:44 -0300

    training js ascii-table

--- cev-challenges/js/pw.js
+++ cev-challenges/js/pw.js
@@ -1,13 +1,23 @@
 const cpw = document.querySelector('#cracked')
 const pw = document.querySelector('#pw')
-
 document.querySelector('#btn').addEventListener('click', crackPassword)
 
 async function crackPassword() {
-    var val = 0
-    while(val != pw.value) {
-        val++
-        console.log(val)
+    var size = pw.value.length;
+
+    var val = ['']
+    if(pw.value != '') {
+        for(var i = size - 1; i >= 0; i--) {
+            for(var j = 0; j < 256; j++) {
+                val[i] = j;
+                val[i] = String.fromCharCode(val[i])
+                var pass = val.toString().replace(/,/g, "")
+                console.log(pass)
+                if(pass == pw.value) {
+                    cpw.value = pass
+                    return
+                }
+            }
+        }
     }
-    cpw.value = val
 }
